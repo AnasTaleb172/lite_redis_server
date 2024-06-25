974993b4ac886e85be8638df8ebe2529ea989d5a
AnasTaleb172 <anas_anast@hotmail.com>
2024-06-25 19:49:45 +0300

    server docstring

--- server/server.py
+++ server/server.py
@@ -1,10 +1,18 @@
+
+"""
+This module is the entry point of the application.
+"""
+
 import socket
 import selectors
 
 from handlers.requestHandler import RequestHandler
-from db.dbAdapter import DbAdapter, LocalDbAdapter, TTLDbAdapter
+from db.dbAdapter import DbAdapter, LocalDbAdapter
 
 class RedisServer:
+    """
+    This class is the TCP server that handles socket initialization and concurrency.
+    """
     def __init__(self, dbAdapter: DbAdapter, host='127.0.0.1', port=6380):
         self.host = host
         self.port = port
@@ -15,6 +23,8 @@ class RedisServer:
         self.connectionsCount = 0
 
     def start(self):
+        """Runs the server to start listening to incoming requests."""
+
         self.sock.bind((self.host, self.port))
         self.sock.listen()
         self.sock.setblocking(False)
@@ -36,17 +46,23 @@ class RedisServer:
             print(f"Total # of connections: {self.connectionsCount}")
         finally:
             self.sock.close()
-    
+
     def _handle_data(self, data: bytes) -> bytes:
+        """Calls the request handler and returns the handled data."""
+
         return self.requestHandler.handle(data)
-        
+
     def accept_connection(self, sock: socket.socket):
+        """Accepts the connection and register it in the global selector."""
+
         conn, addr = sock.accept()
-        # print(f"Accepted connection from {addr}")
+        print(f"Accepted connection from {addr}")
         conn.setblocking(False)
         self.selector.register(conn, selectors.EVENT_READ | selectors.EVENT_WRITE, data=b'')
 
     def serve_connection(self, key, mask):
+        """Handles a connection in case of the socket is ready to read or write."""
+
         sock: socket.socket = key.fileobj
         data = key.data
 
@@ -54,19 +70,15 @@ class RedisServer:
         if mask & selectors.EVENT_READ:
             recv_data = sock.recv(1024)
             if recv_data and recv_data != b'*2\r\n$7\r\nCOMMAND\r\n$4\r\nDOCS\r\n':
-                # print(f"Received data: {recv_data}")
                 data += recv_data
             else:
-                # print(f"Closing connection to ....")
                 self.selector.unregister(sock)
                 sock.close()
 
         # Socket is ready to write -> Read from socket & send to buffer
         if mask & selectors.EVENT_WRITE:
             if data:
-                # print(f"Responding To a client ....")
                 handledData = self._handle_data(data)
-                # print(f"Handled data: {handledData}")
                 sent = sock.send(handledData)
                 data = data[sent:]
 
